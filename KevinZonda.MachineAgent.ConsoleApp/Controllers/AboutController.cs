07d39225b461fee00bec0c93e1e3f5dd724e6f41
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-09-21 10:55:27 +0100

    feat: add about

---
+++ KevinZonda.MachineAgent.ConsoleApp/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace KevinZonda.MachineAgent.ConsoleApp.Controllers;
+
+internal class AboutController
+{
+    public static string GetAboutMessage()
+    {
+        using var proc = Process.GetCurrentProcess();
+
+        return
+            $"KevinZonda.MachineAgent\n" +
+            $"=======================\n" +
+            $"Version  : {Assembly.GetExecutingAssembly().GetName().Version}\n" +
+            $"Runtime  : .NET {Environment.Version}\n" +
+            $"OS       : {Environment.OSVersion}\n" +
+            $"CPU Count: {Environment.ProcessorCount}\n" +
+            $"Memory   : {1.0 * proc.PrivateMemorySize64 / 1024 / 1024} MiB";
+    }
+
+}
--- KevinZonda.MachineAgent.ConsoleApp/Program.cs
+++ KevinZonda.MachineAgent.ConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using KevinZonda.MachineAgent.ConsoleApp.Controllers;
 
+Console.WriteLine(AboutController.GetAboutMessage());
+return;
 var r = await IPController.GetIPInfo();
 
 Console.WriteLine(r.Location);
